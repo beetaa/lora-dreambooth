4e9520e7ab97b4866f6809d88a4c9f6bb9123890
beetaa <beetaa@outlook.com>
2023-05-29 10:39:06 +0800

    update

--- install.bash
+++ install.bash
@@ -1,58 +1,57 @@
 #!/usr/bin/bash
 
 script_dir="/kaggle/working/lora-dreambooth"
-echo "$script_dir"
-cd "$script_dir/sd-scripts"
-ls -al
-# create_venv=true
+cd "$script_dir"
 
-# while [ -n "$1" ]; do
-#     case "$1" in
-#         --disable-venv)
-#             create_venv=false
-#             shift
-#             ;;
-#         *)
-#             shift
-#             ;;
-#     esac
-# done
+create_venv=true
 
-# if $create_venv; then
-#     echo "Creating python venv..."
-#     python3 -m venv venv
-#     source "$script_dir/venv/bin/activate"
-#     echo "active venv"
-# fi
+while [ -n "$1" ]; do
+    case "$1" in
+        --disable-venv)
+            create_venv=false
+            shift
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
 
-# echo "Installing torch & xformers..."
+if $create_venv; then
+    echo "Creating python venv..."
+    python3 -m venv venv
+    source "$script_dir/venv/bin/activate"
+    echo "active venv"
+fi
 
-# cuda_version=$(nvcc --version | grep 'release' | sed -n -e 's/^.*release \([0-9]\+\.[0-9]\+\),.*$/\1/p')
-# cuda_major_version=$(echo "$cuda_version" | awk -F'.' '{print $1}')
-# cuda_minor_version=$(echo "$cuda_version" | awk -F'.' '{print $2}')
+echo "Installing torch & xformers..."
 
-# echo "Cuda Version:$cuda_version"
+cuda_version=$(nvcc --version | grep 'release' | sed -n -e 's/^.*release \([0-9]\+\.[0-9]\+\),.*$/\1/p')
+cuda_major_version=$(echo "$cuda_version" | awk -F'.' '{print $1}')
+cuda_minor_version=$(echo "$cuda_version" | awk -F'.' '{print $2}')
 
-# if (( cuda_major_version >= 12 )) || (( cuda_major_version == 11 && cuda_minor_version >= 8 )); then
-#     echo "install torch 2.0.0+cu118"
-#     pip install torch==2.0.0+cu118 torchvision==0.15.1+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
-#     pip install xformers==0.0.19
-# elif (( cuda_major_version == 11 && cuda_minor_version == 6 )); then
-#     echo "install torch 1.12.1+cu116"
-#     pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116
-#     pip install --upgrade git+https://github.com/facebookresearch/xformers.git@0bad001ddd56c080524d37c84ff58d9cd030ebfd
-#     pip install triton==2.0.0.dev20221202
-# else
-#     echo "Unsupported cuda version:$cuda_version"
-#     exit 1
-# fi
+echo "Cuda Version:$cuda_version"
 
-# echo "Installing deps..."
-# cd "$script_dir/sd-scripts" || exit
+if (( cuda_major_version >= 12 )) || (( cuda_major_version == 11 && cuda_minor_version >= 8 )); then
+    echo "install torch 2.0.0+cu118"
+    pip install torch==2.0.0+cu118 torchvision==0.15.1+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
+    pip install xformers==0.0.19
+elif (( cuda_major_version == 11 && cuda_minor_version == 6 )); then
+    echo "install torch 1.12.1+cu116"
+    pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116
+    pip install --upgrade git+https://github.com/facebookresearch/xformers.git@0bad001ddd56c080524d37c84ff58d9cd030ebfd
+    pip install triton==2.0.0.dev20221202
+else
+    echo "Unsupported cuda version:$cuda_version"
+    exit 1
+fi
 
-# pip install --upgrade -r requirements.txt
-# pip install --upgrade lion-pytorch lycoris-lora dadaptation fastapi uvicorn wandb
+echo "Installing deps..."
+cd "$script_dir/sd-scripts" || exit
 
-# cd "$script_dir" || exit
+pip install --upgrade -r requirements.txt
+pip install --upgrade lion-pytorch lycoris-lora dadaptation fastapi uvicorn wandb
 
-# echo "Install completed"
+cd "$script_dir" || exit
+
+echo "Install completed"
